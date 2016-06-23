c2a04dd6089bd8a9ae9dde8f61d76f6ab6c378a9
david <dakrauth@gmail.com>
2016-06-23 17:53:57 -0400

    abstract demo updates

---
+++ abstract_demo/abstract/settings.py
@@ -0,0 +1,87 @@
+import os, sys
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(BASE_DIR))
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'upnc#7_z7azt&clxzix5wcj5(d*7-e6g#7gx+_%-+zk4(zurn1'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = []
+ROOT_URLCONF = 'abstract.urls'
+WSGI_APPLICATION = 'abstract.wsgi.application'
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'rrule',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swingtime',
+        'USER': 'root'
+    }
+}
+
+if 1:
+    DATABASES['default'].update(ENGINE='django.db.backends.sqlite3', NAME='rrule.sqlite')
+
+print DATABASES
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+
+# Internationalization https://docs.djangoproject.com/en/1.9/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/'
---
+++ abstract_demo/rrule/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.db import models
+
+from swingtime import models as swingtime
+
+class RRuleEventType(swingtime.EventTypeBase):
+	pass
+
+class RRuleEvent(swingtime.EventBase):
+    event_type = models.ForeignKey(RRuleEventType, verbose_name='event type')
+
+
+create_rrule_event = swingtime.event_creation_factory(RRuleEvent, RRuleEventType)
